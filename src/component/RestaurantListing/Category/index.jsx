f7244611aa1614da1076f3584d9e9468f43739dc
Ayobami Omotosho <ayobamimoses1@gmail.com>
2019-10-11 16:19:31 +0100

    first commit

--- src/component/RestaurantListing/Category/index.jsx
+++ src/component/RestaurantListing/Category/index.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import CheckBox from '../../common/Checkbox';
 import AppTextHeader from '../../common/AppTextHeader';
 
 import './styles.scss'
+import AppText from '../../common/AppText';
 
-const Category = (props) => {
+const Category = ({ categoryList }) => {
 
-    const list = [1, 2] || props.category;
-    const checkBoxList = list.map((category, index) => <CheckBox key={index}/>);
+    const checkBoxList = categoryList.length ? categoryList.map(
+        (category, index) => <CheckBox checkedValue={category.id} checkboxLabel={category.name} key={index}/>
+        ) : <AppText text="No Categories found" appTextExtraCSS="text-center" />;
 
     return (
-        <div className="category app-bg">
-            <AppTextHeader appTextHeaderExtraCSS='h6' text='Category'/>
-            {checkBoxList}
+        <div className="app-bg">
+            <div className="row mb-2">
+                <div className="col">
+                    <div className="p-3">
+                        <AppTextHeader appTextHeaderExtraCSS='h6 text-center' text='Category'/>
+                        <div>
+                            {checkBoxList}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
- 
+
+// Category.defaultProps = {
+//     categoryList: []
+// }
+
+Category.defaultProps = {
+    categoryList: [{ id: 1, name: 'Delivery' }, { id: 2, name: 'Dine-out' }]
+}
+
+Category.propTypes = {
+    categoryList: PropTypes.array
+}
+
 export default Category;
