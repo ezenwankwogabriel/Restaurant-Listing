de27e9491668842883060efd588a0a069fe5b39c
Ezenwankwo <ezenwankwogabriel@gmail.com>
2019-10-13 20:24:37 +0100

    Added card component Added Test for card components

--- src/component/RestaurantListing/RestaurantSearchResult/restaurantSearchResult.test.js
+++ src/component/RestaurantListing/RestaurantSearchResult/restaurantSearchResult.test.js
@@ -1,42 +1,56 @@
 import React from "react";
-import { shallow } from 'enzyme';
+import { shallow } from "enzyme";
 import RestaurantSearchResult from "./";
+import Cards from './cards'
 import { findByTestAttr, checkProps } from "../../../utils";
 
+const restuarantSample = {
+  image:
+    "https://res.cloudinary.com/nesterpod-com/image/upload/v1570649796/1_Drinks_gtdgfr.png",
+  link: "View More",
+  title: "Card Title",
+  content:
+    "Some quick example text to build on the card title and make up the bulk of the cards content.",
+};
 const expectedProps = {
-  results: [{
-    image: 'https://res.cloudinary.com/nesterpod-com/image/upload/v1570649796/1_Drinks_gtdgfr.png',
-    link: 'View More',
-    title: 'Card Title', 
-    content: 'Some quick example text to build on the card title and make up the bulk of the cards content.', 
-    id: 1,
-  }]
+  results: [{...restuarantSample, id: 1}],
 };
 
-const setup = (props={}) => {
-    const componentProps= {
-        ...expectedProps,
-        ...props
-    };
+const setup = (props = {}) => {
+  const componentProps = {
+    ...expectedProps,
+    ...props
+  };
 
-    let component = shallow(<RestaurantSearchResult {...componentProps} />);
-    return component;
+  let component = shallow(<RestaurantSearchResult {...componentProps} />);
+  return component;
 };
 
-
-describe('Restaurant Result Component', () => {
-    describe('Checking propTypes', () => {
-        it('Should not throw error', () => {
-            const restaurantError = checkProps(RestaurantSearchResult, expectedProps);
-            expect(restaurantError).toBeUndefined();
-        });
+describe("Restaurant Result Component", () => {
+  describe("Checking propTypes", () => {
+    it("Should not throw error", () => {
+      const restaurantError = checkProps(RestaurantSearchResult, expectedProps);
+      expect(restaurantError).toBeUndefined();
     });
+  });
 
-    describe('Restaurant List render', () => {
-        let wrapper = setup();
-        it('Should render restaurant without an error', () => {
-            const restaurantComponent = findByTestAttr(wrapper, 'app-results');
-            expect(restaurantComponent.length).toBe(1);
-        });
+  describe("Restaurant List render", () => {
+    let wrapper = setup();
+    it("Should render restaurant without an error", () => {
+      const restaurantComponent = findByTestAttr(wrapper, "app-results");
+      expect(restaurantComponent.length).toBe(1);
     });
-})
+  });
+
+  describe("Restaurant List render", () => {
+    it("Should render restaurant with 2 Cards as passed on props", () => {
+      let wrapper = setup({
+        results: [
+          {...restuarantSample, id: 1},
+          {...restuarantSample, id: 2},
+        ]
+      });
+      expect(wrapper.find(Cards)).toHaveLength(2);
+    });
+  });
+});
--- src/component/RestaurantListing/index.jsx
+++ src/component/RestaurantListing/index.jsx
@@ -9,12 +9,20 @@ import RestaurantSearchBox from "./RestaurantSearchBox";
 
 const RestaurantListing = () => {
 
-    const sampleData = [{
-        imageUrl: 'https://res.cloudinary.com/nesterpod-com/image/upload/v1570649796/1_Drinks_gtdgfr.png',
-        cardTitle: 'Card Title', 
-        cardContent: 'Some quick example text to build on the card title and make up the bulk of the cards content.', 
-        id: 1
-      }];
+  const listing = {
+    imageUrl: 'https://res.cloudinary.com/nesterpod-com/image/upload/v1570649796/1_Drinks_gtdgfr.png',
+    cardTitle: 'Card Title', 
+    cardContent: 'Some quick example text to build on the card title and make up the bulk of the cards content.', 
+    id: 1
+  };
+    const sampleData = [
+      {...listing, id: 1},
+      {...listing, id: 2},
+      {...listing, id: 3},
+      {...listing, id: 4},
+      {...listing, id: 5},
+      {...listing, id: 6}
+    ];
     return (
         <div className="listings">
             <div className="row">
