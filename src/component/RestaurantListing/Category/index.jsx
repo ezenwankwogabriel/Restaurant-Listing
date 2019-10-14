225db78ed97021068cc0343d688cf1b91099febf
Ayobami Omotosho <ayobamimoses1@gmail.com>
2019-10-14 10:41:29 +0100

    category section done

--- src/component/RestaurantListing/Category/index.jsx
+++ src/component/RestaurantListing/Category/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import CheckBox from '../../common/Checkbox';
@@ -9,9 +9,24 @@ import AppText from '../../common/AppText';
 
 const Category = ({ categoryList }) => {
 
+    const [checkedCategories, setCheckedCategories] = useState([]);
+
+    function onChange(e) {
+        const checkedValue = e.target.value;
+        checkedCategories[checkedValue] = checkedValue
+
+        setCheckedCategories(prev => {
+            return prev=checkedCategories
+        });
+    }
+
     const checkBoxList = categoryList.length ? categoryList.map(
-        (category, index) => <CheckBox checkedValue={category.id} checkboxLabel={category.name} key={index}/>
-        ) : <AppText text="No Categories found" appTextExtraCSS="text-center" />;
+        (category, index) => (
+            <div className="mb-1" key={index}>
+                <CheckBox checkedValue={`${category.id}`} onChange={onChange} isChecked={checkedCategories[category.id] === category.id} checkboxLabel={category.name}/>
+            </div>
+        )
+    ) : <AppText text="No Categories found" appTextExtraCSS="text-center" />;
 
     return (
         <div className="app-bg">
@@ -29,13 +44,13 @@ const Category = ({ categoryList }) => {
     );
 }
 
-// Category.defaultProps = {
-//     categoryList: []
-// }
-
 Category.defaultProps = {
-    categoryList: [{ id: 1, name: 'Delivery' }, { id: 2, name: 'Dine-out' }]
+    categoryList: []
 }
+
+// Category.defaultProps = {
+//     categoryList: [{ id: 1, name: 'Delivery' }, { id: 2, name: 'Dine-out' }]
+// }
 
 Category.propTypes = {
     categoryList: PropTypes.array
