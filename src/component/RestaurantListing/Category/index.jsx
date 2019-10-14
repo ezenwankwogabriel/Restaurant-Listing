a7c2f3806491a030bac6047b1c23091e3b611e2a
Ayobami Omotosho <ayobamimoses1@gmail.com>
2019-10-14 15:07:41 +0100

    app section category test update

--- src/component/RestaurantListing/Category/index.jsx
+++ src/component/RestaurantListing/Category/index.jsx
@@ -23,13 +23,13 @@ const Category = ({ categoryList }) => {
     const checkBoxList = categoryList.length ? categoryList.map(
         (category, index) => (
             <div className="mb-1" key={index}>
-                <CheckBox checkedValue={`${category.id}`} onChange={onChange} isChecked={checkedCategories[category.id] === category.id} checkboxLabel={category.name}/>
+                <CheckBox data-testid="app-category-item" checkedValue={`${category.id}`} onChange={onChange} isChecked={checkedCategories[category.id] === category.id} checkboxLabel={category.name}/>
             </div>
         )
-    ) : <AppText text="No Categories found" appTextExtraCSS="text-center" />;
+    ) : <AppText data-testid="no-category" text="No Categories found" appTextExtraCSS="text-center" />;
 
     return (
-        <div className="app-bg">
+        <div className="app-bg" data-testid="app-category">
             <div className="row mb-2">
                 <div className="col">
                     <div className="p-3">
@@ -47,10 +47,6 @@ const Category = ({ categoryList }) => {
 Category.defaultProps = {
     categoryList: []
 }
-
-// Category.defaultProps = {
-//     categoryList: [{ id: 1, name: 'Delivery' }, { id: 2, name: 'Dine-out' }]
-// }
 
 Category.propTypes = {
     categoryList: PropTypes.array
