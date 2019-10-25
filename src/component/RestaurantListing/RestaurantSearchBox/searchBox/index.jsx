9ed3d633d14c3f5d887ef98def61ed280531d505
Ezenwankwo <ezenwankwogabriel@gmail.com>
2019-10-25 12:51:04 +0100

    Modified the component to handle searching for restaurants

--- src/component/RestaurantListing/RestaurantSearchBox/searchBox/index.jsx
+++ src/component/RestaurantListing/RestaurantSearchBox/searchBox/index.jsx
@@ -5,24 +5,29 @@ import './styles.scss';
 const SearchBox = ({ value, result, onChange, message }) => {
 
   function displayList() {
-    let locations = [];
-    locations = value && result.filter((location) => location.city_name.toLowerCase().match(value));
-    if (locations.length > 0)
-      return (locations.map((location, index) => (<div data-testid="app-search-item" className="dropDownList" key={index}>{location.title}</div>)));
-    return (<div>{message}</div>)
+    if (result.length > 0 && message !== 'Fetching locations ... ')
+      return (
+          result.map((location, index) => (
+            <option data-testid="app-search-item" className="dropDownList" key={index}>{location.title}</option>)
+          )
+        );
+    return (<option data-testid="app-search-item" className="dropDownList">{message}</option>)
   }
 
   return (
     <div className="listDiv">
-      <input data-testid="app-search-box" className="form-control" value={value} type="text" autoComplete="no" onChange={(event) => onChange(event)}/>
-      <ul className="">{displayList()}</ul>
+      <input list="browsers" name="browsers" data-testid="app-search-box" className="form-control" value={value} type="text" autoComplete="no" onChange={(event) => onChange(event)}/> 
+      <datalist id="browsers" className="dropDown truncate">
+        {displayList()}
+      </datalist>
+      {/* <ul className="dropDown truncate" >{displayList()}</ul> */}
     </div>
   )
 }
  
 SearchBox.propTypes = {
-  value: PropTypes.string.isRequired,
-  result: PropTypes.array.isRequired,
+  value: PropTypes.string,
+  result: PropTypes.array,
   onChange: PropTypes.func.isRequired,
   message: PropTypes.string,
 }
